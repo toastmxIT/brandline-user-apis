b3c8c9ff33c95ca38942b2650136ff7c6371ce8e
Pedro Orellana <pedro.orellana@credifranco.com>
2019-11-03 16:52:28 -0600

    refactor: add comments on deployment file

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 sam build --use-container
 sam package --output-template packaged.yaml --s3-bucket brandline-bucket
 sam deploy --template-file packaged.yaml --capabilities CAPABILITY_IAM --stack-name brandline-apis
+#deployment file
